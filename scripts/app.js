ba163dfa752cb7b2963fae698c66f543e19c7167
nmkhans <nurmoin05@gmail.com>
2025-03-03 22:02:25 +0600

    image change based on color feature added

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,10 @@
 const ringButtons = document.querySelectorAll(".ring-button");
+const imageContainer = document.querySelector("#product-image");
 
 function handleColorButtonClick(e) {
   const clickedBtn = e.target;
+  const color = clickedBtn.id.replace("-color", "");
+  console.log(color)
 
   for (const btn of ringButtons) {
     if (btn.classList.contains("border-purple-600")) {
@@ -10,7 +13,10 @@ function handleColorButtonClick(e) {
     btn.classList.add("border-gray-300");
   }
 
+  clickedBtn.classList.remove("border-gray-300");
   clickedBtn.classList.add("border-purple-600");
+
+  imageContainer.src = `../images/${color}.png`
 }
 
 for (const button of ringButtons) {
