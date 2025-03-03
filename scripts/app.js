4e1c80cbf69f1f29e469fbb0a484803acc8d3702
nmkhans <nurmoin05@gmail.com>
2025-03-03 23:25:02 +0600

    wrist size select feature added

--- scripts/app.js
+++ scripts/app.js
@@ -4,7 +4,6 @@ const imageContainer = document.querySelector("#product-image");
 function handleColorButtonClick(e) {
   const clickedBtn = e.target;
   const color = clickedBtn.id.replace("-color", "");
-  console.log(color)
 
   for (const btn of ringButtons) {
     if (btn.classList.contains("border-purple-600")) {
@@ -16,9 +15,25 @@ function handleColorButtonClick(e) {
   clickedBtn.classList.remove("border-gray-300");
   clickedBtn.classList.add("border-purple-600");
 
-  imageContainer.src = `../images/${color}.png`
+  imageContainer.src = `../images/${color}.png`;
 }
 
 for (const button of ringButtons) {
   button.addEventListener("click", handleColorButtonClick);
+}
+
+function handleWristSizeSelection(size) {
+  const sizes = ["S", "M", "L", "XL"];
+
+  for (const currentSize of sizes) {
+    const button = document.querySelector(`#size-${currentSize}`);
+
+    if (currentSize === size) {
+      button.classList.remove("border-gray-300");
+      button.classList.add("border-purple-600");
+    } else {
+      button.classList.remove("border-purple-600");
+      button.classList.add("border-gray-300");
+    }
+  }
 }
